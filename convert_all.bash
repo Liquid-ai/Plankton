9bcbe08961b3480d4f29e1e9dc769c12e443958b
Anthony Rossi <thony.rossi@gmail.com>
2020-03-19 14:07:53 +0100

    updated script

--- convert_all.bash
+++ convert_all.bash
@@ -2,7 +2,13 @@ process_dir() {
     for d in $1/*; do
     if [ -d "$d" ]; then
         if [ -f "$d/package.xml" ]; then
-            (cd $d && sed -i "/^\w*#/d" CMakeLists.txt && python3 /home/slock/Documents/catkin-to-ament.py)
+            if grep -Fxq "# Add support for C++11" "$d/CMakeLists.txt"
+            then
+                echo "$d was already processed, skipping"
+            else
+                echo "processing $d"
+                (cd $d && sed -i "/^\w*#/d" CMakeLists.txt && python3 /home/slock/Documents/catkin-to-ament.py)
+            fi
         fi
         process_dir $d
     fi
