8a7e2a3b1cf598d584d88e952bfcf1e22d8d758d
jpliquid <65894393+jpliquid@users.noreply.github.com>
2020-08-04 16:58:19 +0200

    Updated tuorials package

--- uuv_tutorials/uuv_tutorial_dp_controller/scripts/tutorial_dp_controller.py
+++ uuv_tutorials/uuv_tutorial_dp_controller/scripts/tutorial_dp_controller.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 # Import the ROS Python library
-import rospy
+import rclpy
 # Import the NumPy package for numerical computations
 import numpy as np
 # Import the dynamic positioning controller base class to inherit methods such as error computation update,
@@ -44,10 +44,10 @@ class TutorialDPController(DPControllerBase):
     #
     # where e is the pose error vector, in this case defined as e = (x, y, z, roll, pitch, yaw)^T
 
-    def __init__(self):
+    def __init__(self, node_name):
         # Calling the constructor of the super-class DPControllerBase, which has the implementation of the error
         # computation update and to publish the resulting torque control vector.
-        super(TutorialDPController, self).__init__(self)
+        super(TutorialDPController, self).__init__(self, node_name)
 
         # The controller should read its parameters from the ROS parameter server for initial setup
         # One way to do this is to read the parameters from the node's private parameter namespace, which is done by
@@ -77,32 +77,33 @@ class TutorialDPController(DPControllerBase):
         # Now the gain matrices need to be set according to the variables stored in the parameter server
         # For simplicity, the gain matrices are defined as diagonal matrices, so only 6 coefficients are
         # needed
-        if rospy.get_param('~Kp'):
-            Kp_diag = rospy.get_param('~Kp')
+        # Bug fix from original code
+        if self.has_parameter('~Kp'):
+            Kp_diag = self.get_parameter('~Kp').get_parameter_value().double_array_value
             if len(Kp_diag) == 6:
                 self._Kp = np.diag(Kp_diag)
             else:
                 # If the vector provided has the wrong dimension, raise an exception
-                raise rospy.ROSException('For the Kp diagonal matrix, 6 coefficients are needed')
+                raise RuntimeError('For the Kp diagonal matrix, 6 coefficients are needed')
 
         # Do the same for the other two matrices
-        if rospy.get_param('~Kd'):
-            diag = rospy.get_param('~Kd')
+        if self.has_parameter('~Kd'):
+            diag = self.get_parameter('~Kd').get_parameter_value().double_array_value
             if len(diag) == 6:
                 self._Kd = np.diag(diag)
                 print 'Kd=\n', self._Kd
             else:
                 # If the vector provided has the wrong dimension, raise an exception
-                raise rospy.ROSException('For the Kd diagonal matrix, 6 coefficients are needed')
+                raise RuntimeError('For the Kd diagonal matrix, 6 coefficients are needed')
 
-        if rospy.get_param('~Ki'):
-            diag = rospy.get_param('~Ki')
+        if self.has_parameter('~Ki'):
+            diag = self.get_parameter('~Ki').get_parameter_value().double_array_value
             if len(diag) == 6:
                 self._Ki = np.diag(diag)
                 print 'Ki=\n', self._Ki
             else:
                 # If the vector provided has the wrong dimension, raise an exception
-                raise rospy.ROSException('For the Ki diagonal matrix, 6 coefficients are needed')
+                raise RuntimeError('For the Ki diagonal matrix, 6 coefficients are needed')
             self._is_init = True
 
     def _reset_controller(self):
@@ -142,7 +143,7 @@ class TutorialDPController(DPControllerBase):
         # will then distribute the efforts amongst the thrusters using the thruster allocation matrix
         self.publish_control_wrench(tau)
 
-if __name__ == '__main__':
+def main():
     # Since this is an ROS node, this Python script has to be treated as an executable
     # Remember to convert this Python file into an executable. This can be done with
     #
@@ -153,13 +154,15 @@ if __name__ == '__main__':
     # After the line catkin_package() in the CMakeLists.txt, include the following
     #
     # catkin_install_python(PROGRAMS scripts/tutorial_dp_controller.py DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION})
-
     print('Tutorial - DP Controller')
-    rospy.init_node('tutorial_dp_controller')
+    rclpy.init()
 
     try:
-        node = TutorialDPController()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        print('caught exception')
+        node = TutorialDPController('tutorial_dp_controller')
+        rclpy.spin(node)
+    except Exception as e:
+        print('Caught exception: ' + str(e))
     print('exiting')
+
+if __name__ == '__main__':
+    main()
