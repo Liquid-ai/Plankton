c1c6a9e8db50537e63816eb2735299935c0b170c
jpliquid <65894393+jpliquid@users.noreply.github.com>
2020-08-24 02:40:52 +0200

    Bug fix for Time to float seconds conversion function

--- plankton_utils/src/time.py
+++ plankton_utils/src/time.py
@@ -1,5 +1,6 @@
 import rclpy
 
 def time_in_float_sec(time: rclpy.time.Time):
-    f_time = time.seconds_nanoseconds[0] + time.seconds_nanoseconds[1] / 1e9
+    sec_nano = time.seconds_nanoseconds()
+    f_time = sec_nano[0] + sec_nano[1] / 1e9
     return f_time
