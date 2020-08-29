3355ffe0fb716484a1598c0e7b5c3d521bccc39f
jpliquid <65894393+jpliquid@users.noreply.github.com>
2020-08-29 15:47:47 +0200

    Added a new time conversion function

--- plankton_utils/src/time.py
+++ plankton_utils/src/time.py
@@ -2,11 +2,28 @@ import rclpy.time
 from rclpy.clock import ClockType
 
 def time_in_float_sec(time: rclpy.time.Time):
+    """
+    From a Time object, returns the associated time in float seconds
+    """
     sec_nano = time.seconds_nanoseconds()
     f_time = sec_nano[0] + sec_nano[1] / 1e9
     return f_time
 
 #==============================================================================
 def time_in_float_sec_from_msg(time_msg, clock_type=ClockType.ROS_TIME):
+    """
+    From a Time message, returns the associated time in float seconds
+    """
     time_object = rclpy.time.Time.from_msg(time_msg, clock_type)
     return time_in_float_sec(time_object)
+
+#==============================================================================
+def float_sec_to_int_sec_nano(float_sec):
+    """
+    From a floating value in seconds, returns a tuple of integer seconds and
+    nanoseconds
+    """
+    secs = int(float_sec)
+    nsecs = int((float_sec - secs) * 1e9)
+
+    return (secs, nsecs)
