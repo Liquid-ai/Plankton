d533a9a3d4acc7ac27fb85c8058a2765e33bac74
jpliquid <65894393+jpliquid@users.noreply.github.com>
2020-09-07 02:32:02 +0200

    Migrated sensor_ros_plugins tests

--- uuv_sensor_plugins/uuv_sensor_ros_plugins/test/test_urdf_files.py
+++ uuv_sensor_plugins/uuv_sensor_ros_plugins/test/test_urdf_files.py
@@ -13,17 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import print_function
-import rospy
 import unittest
 import subprocess
 import os
-
-PKG = 'uuv_sensor_ros_plugins'
-NAME = 'test_urdf_files'
-
-import roslib
-roslib.load_manifest(PKG)
 
 
 def call_xacro(xml_file):
@@ -31,6 +23,7 @@ def call_xacro(xml_file):
     return subprocess.check_output(['xacro', '--inorder', xml_file])
 
 
+# =============================================================================
 class TestURDFFiles(unittest.TestCase):
     def test_xacro(self):
         # Retrieve the root folder for the tests
@@ -42,17 +35,17 @@ class TestURDFFiles(unittest.TestCase):
                 continue
             output = call_xacro(os.path.join(robots_dir, item))
             self.assertNotIn(
-                output, 
+                output.decode('utf-8'), 
                 'XML parsing error',
                 'Parsing error found for file {}'.format(item))
             self.assertNotIn(
-                output, 
+                output.decode('utf-8'), 
                 'No such file or directory', 
                 'Some file not found in {}'.format(item))
 
-if __name__ == '__main__':
-    import rosunit
-    rosunit.unitrun(PKG, NAME, TestURDFFiles)
+# if __name__ == '__main__':
+#     import rosunit
+#     rosunit.unitrun(PKG, NAME, TestURDFFiles)
 
 
 
