1b03f249b262beb3aff185fa89635a321bf0e8a7
Loic <51138840+LoicLiquid@users.noreply.github.com>
2020-09-21 22:10:39 +0200

    Update copyright

--- uuv_assistants/src/message_to_tf.cpp
+++ uuv_assistants/src/message_to_tf.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2016 The UUV Simulator Authors.
-// All rights reserved.
+// Copyright (c) 2020 The Liquid.ai Authors
+// All rights reserved
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
