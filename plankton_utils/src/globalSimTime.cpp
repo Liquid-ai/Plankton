6713b9b487ce62733354ca215374d5d743960d06
jpliquid <65894393+jpliquid@users.noreply.github.com>
2020-09-28 01:41:38 +0200

    Added a node to serve as a reference for sim time in the simulation

---
+++ plankton_utils/src/globalSimTime.cpp
@@ -0,0 +1,13 @@
+#include <rclcpp/rclcpp.hpp>
+
+/// Listens for nodes asking if the simulation is using sim_time and replies
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    
+    auto node = rclcpp::Node::make_shared("plankton_global_sim_time");
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    
+    return 0;
+}
